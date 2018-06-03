eb4f57f1e58683f97f6f8aeaf712381c07b2eca9
Kubo <t_nutji@hotmail.com>
2018-06-03 13:34:25 +0700

    FunnyNumber1

---
+++ FunnyNumber/EnglishViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EnglishViewController.swift
+//  FunnyNumber
+//
+//  Created by Tanut Khositprasert on 3/6/2561 BE.
+//  Copyright © 2561 Tanut Khositprasert. All rights reserved.
+//
+
+import UIKit
+
+class EnglishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
